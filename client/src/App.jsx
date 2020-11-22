8ea1faa3689114af1c0176f41628285dae4d4f4c
xsebastianx <sarnavat1@gmail.com>
2020-11-22 17:01:27 -0500

    Initial navbar work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
+import Navbar from './components/HOMEPAGE/Navbar/Navbar';
 
 import './App.css';
 
